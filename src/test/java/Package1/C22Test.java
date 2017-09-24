aae2d9ef21352c808c6d21f925a3174b9a675cfe
Guillermo Sanz <Guillermo Sanz@DESKTOP-VQF1VN7>
2017-09-24 20:43:26 +0200

    Create Class C22Test

---
+++ src/test/java/Package1/C22Test.java
@@ -0,0 +1,13 @@
+package Package1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class C22Test {
+	@Test
+	public void mATest() {
+		C22 tester = new C22();
+		assertEquals("mA", tester.mA());
+	}
+}
