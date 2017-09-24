0ae3401ef1885dcb8b449c85626a1a9946fbe899
Guillermo Sanz <Guillermo Sanz@DESKTOP-VQF1VN7>
2017-09-24 20:37:29 +0200

    issue#02 Create Class C21Test

---
+++ src/test/java/Package1/C21Test.java
@@ -0,0 +1,18 @@
+package Package1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class C21Test {
+	@Test
+	public void m1Test() {
+		C21 tester = new C21();
+		assertEquals("m1", tester.m1());
+	}
+	@Test
+	public void m2Test() {
+		C21 tester = new C21();
+		assertEquals("m2", tester.m2());
+	}
+}
